92796244b47f092a48955b7d061660340ba47814
Your Name <you@example.com>
2017-02-25 12:23:19 +0000

    time efficiency improvment: Replaced the Polyrex gem with the PxLite gem which can iterate over records much faster.

--- bankstatement_txt.gemspec
+++ bankstatement_txt.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'bankstatement_txt'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Experimental gem for querying a running bank statement.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/bankstatement_txt.rb']
-  s.add_runtime_dependency('polyrex', '~> 0.10', '>=0.10.2')
+  s.add_runtime_dependency('pxlite', '~> 0.1', '>=0.1.0')
+  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.20')
   s.signing_key = '../privatekeys/bankstatement_txt.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/bankstatement_txt.rb
+++ lib/bankstatement_txt.rb
@@ -3,7 +3,8 @@
 # file: bankstatement_txt.rb
 
 require 'csv'
-require 'polyrex'
+require 'pxlite'
+require 'dynarex'
 
 
 class BankStatementTxt
@@ -18,6 +19,15 @@ class BankStatementTxt
     new_import(filepath)
     
   end
+  
+  def inspect()
+    "<bankstatement #{object.id}>"
+  end
+  
+  def summary()    
+    @dx
+  end
+  
 
   private
 
@@ -34,40 +44,72 @@ class BankStatementTxt
 
     a3 = a2.inject({}) do |r, pair| 
 
-      year, x = pair
-      
+      year, x = pair      
       r.merge(year => x.group_by {|y| Date.parse(y[:date]).month})
+      
     end
 
-    px = Polyrex.new("statement[title,tags]/year[title]/month[title]/entry" + 
-                     "[date, type, desc, credit, debit, balance, tags]")
+    px = PxLite.new("statement[title,tags]/year[title]/month[title, " + 
+                     "credit, debit]/entry[date, type, desc, credit, " + 
+                     "debit, balance, tags]")    
+    
+    rec = []
 
     a3.each do |year, year_entries|
+      
+      rec << yearx = [{title: year.to_s}, []]
 
-      px.create_year(title: year.to_s) do |create1|
+      year_entries.each do |month, entries|
 
-        year_entries.each do |month, entries|
+        yearx.last << monthx = [{title: Date::ABBR_MONTHNAMES[month]}, []]
 
-          create1.month(title: Date::ABBR_MONTHNAMES[month]) do |create2|
+        entries.each do |x|
 
-            entries.each do |x|
-
-              v = x[:value]
-              credit, debit = v > 0 ? [v, nil] : [nil, v.abs]
-              create2.entry date: x[:date], type: x[:type], 
-                            desc: x[:description], credit: credit, 
-                            debit: debit, balance: x[:balance]
-
-            end
-
-          end
-
+          v = x[:value]
+          credit, debit = v > 0 ? [v, nil] : [nil, v.abs]
+          monthx.last << [{date: x[:date], type: x[:type], 
+                        desc: x[:description], credit: credit, 
+                        debit: debit, balance: x[:balance]}, []]
         end
+
+      end
+
+    end
+    
+    # totalise each the credit and debit columns for each month
+    
+    rec.each do |year, months|
+      months.each do |month, entries|
+        month[:credit] = entries.map {|x| x[0][:credit].to_f }.inject(:+)
+        month[:debit] =  entries.map {|x| x[0][:debit].to_f }.inject(:+)
       end
     end
+    
+    
+    # create a Dynarex document for the totals for each month
+    
+    @dx = Dynarex.new('months[title]/month(uid, year, month, ' + 
+                      'debit, credit, net_income)')
+
+    rec.each do |year, months|
+      
+      months.each do |month, entries|
+
+        debit, credit = month[:debit], month[:credit]
+        
+        @dx.create year: year[:title], month: month[:title], 
+          debit: debit.round(2), credit: credit.round(2), 
+          net_income: (credit - debit).round(2)
+
+      end
+      
+    end    
+    
+    
+    px.records = rec
 
     @px = px 
-
+    
   end
 
 end
